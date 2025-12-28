49f5098ed12aeadc696c33c8a9945c0608555a19
rahulkuma9503-coder <rahulkuma9503@gmail.com>
2025-12-28 22:57:08 +0530

    Update __init__.py

--- AnnieXMedia/__init__.py
+++ AnnieXMedia/__init__.py
@@ -1,9 +1,10 @@
-﻿# Authored By Certified Coders © 2025
+# Authored By Certified Coders © 2025
 from AnnieXMedia.core.bot import MusicBotClient
 from AnnieXMedia.core.dir import StorageManager
 from AnnieXMedia.core.git import git
 from AnnieXMedia.core.userbot import Userbot
 from AnnieXMedia.misc import dbb, heroku
+from flask import Flask
 
 from .logging import LOGGER
 
@@ -15,6 +16,20 @@ heroku()
 app = MusicBotClient()
 userbot = Userbot()
 
+# Create Flask web app for Render
+web_app = Flask(__name__)
+
+@web_app.route('/')
+def home():
+    return "Bot is running", 200
+
+@web_app.route('/health')
+def health():
+    return "OK", 200
+
+@web_app.route('/ping')
+def ping():
+    return "pong", 200
 
 from .platforms import *
 
